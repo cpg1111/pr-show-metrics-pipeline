823585fa12de2f88ae4b697954330c613405240d
Christian Grabowski <christiang11754@gmail.com>
2016-12-19 21:08:31 -0500

    create httpServer

--- pkg/httpServer/server.go
+++ pkg/httpServer/server.go
@@ -1,1 +1,18 @@
 package httpServer
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Server struct {
+	http.Server
+}
+
+func New(proto, host string, port int) (*Server, error) {
+	mux := http.NewServeMux()
+	return &Server{
+		Addr:    fmt.Sprintf("%s://%s:%d", proto, host, port),
+		Handler: mux,
+	}
+}
